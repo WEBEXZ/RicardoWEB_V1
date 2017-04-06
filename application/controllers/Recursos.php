6d7fdbbdbc0687cf60491848361c56e59c582a26
Carlos Carrera <WEBEXZ@users.noreply.github.com>
2017-04-06 12:27:51 -0500

    Agregando Estados y Municipios

--- application/controllers/Recursos.php
+++ application/controllers/Recursos.php
@@ -8,13 +8,31 @@ class Recursos extends CI_Controller {
 	   $this->load->model('EstadoModel');
 	   $this->load->model('MunicipioModel');
 	 }
- 
+ 	
+ 	 // url/Recursos/getPaises
 	 public function getPaises()
 	 {
 	    $datos = $this->PaisModel->getPaises();
 	    header('Content-Type: application/json');
     	echo json_encode($datos);
 	 }
+
+	 // url/Recursos/getEstados/1
+	 public function getEstados($idPais)
+	 {
+	    $datos = $this->EstadoModel->getEstadosByPais($idPais);
+	    header('Content-Type: application/json');
+    	echo json_encode($datos);
+	 }
+
+	 // url/Recursos/getMunicipios/1/20
+	 public function getMunicipios($idPais, $idEstado)
+	 {
+	    $datos = $this->MunicipioModel->getMunicipiosByEstado($idPais, $idEstado);
+	    header('Content-Type: application/json');
+    	echo json_encode($datos);
+	 }
+
 }
  
 ?>
